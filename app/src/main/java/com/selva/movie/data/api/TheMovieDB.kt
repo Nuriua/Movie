d3f4a334c0a678e11ba6148948094bbbcb33ac6d
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-09 11:30:31 +0500

    TheMovieDB modified

--- app/src/main/java/com/selva/movie/data/api/TheMovieDB.kt
+++ app/src/main/java/com/selva/movie/data/api/TheMovieDB.kt
@@ -2,10 +2,14 @@ package com.selva.movie.data.api
 
 import okhttp3.Interceptor
 
-const val API_KEY
+const val API_KEY = "f7fff1e65e56cde9108b014001a69196"
+const val BASE_URL = ""
+const val POSTER_BASE_URL = ""
+
 object TheMovieDB {
-    fun getClient(): TheMovieDBInterface{
+    fun getClient(): TheMovieDBInterface {
         val requestInterceptor = Interceptor { chain ->
+
             val url = chain.request()
                 .url()
                 .newBuilder()
@@ -15,6 +19,9 @@ object TheMovieDB {
             val request = chain.request()
                 .newBuilder()
                 .url(url)
-        }
+                .build()
 
+            return@Interceptor chain.proceed(request)
+        }
+    }
 }
