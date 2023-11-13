760f68c818a37a0a15ac6197e42fca7cae0aacc5
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-13 19:13:04 +0500

    TheMovieDBInterface modified

--- app/src/main/java/com/selva/movie/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/selva/movie/data/api/TheMovieDBInterface.kt
@@ -1,6 +1,7 @@
 package com.selva.movie.data.api
 
 import com.selva.movie.data.vobject.MovieDetails
+import com.selva.movie.data.vobject.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,7 +9,7 @@ import retrofit2.http.Query
 
 interface TheMovieDBInterface {
     @GET("movie/popular")
-    fun getPopularMovie(@Query("page")page: Int)
+    fun getPopularMovie(@Query("page")page: Int): Single<MovieResponse>
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
 }
