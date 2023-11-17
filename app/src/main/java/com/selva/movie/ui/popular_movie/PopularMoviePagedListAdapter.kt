98bab254c821ad330ecabca04be8bacc26d81383
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-17 11:36:52 +0500

    popularmoviepagedlistadapter added

--- app/src/main/java/com/selva/movie/ui/popular_movie/PopularMoviePagedListAdapter.kt
+++ app/src/main/java/com/selva/movie/ui/popular_movie/PopularMoviePagedListAdapter.kt
@@ -10,6 +10,18 @@ import com.bumptech.glide.Glide
 import com.selva.movie.data.api.POSTER_BASE_URL
 import com.selva.movie.data.vobject.Movie
 
+import android.content.Intent
+import android.view.LayoutInflater
+import androidx.core.content.ContextCompat.startActivity
+
+import com.selva.movie.R
+import com.selva.movie.data.api.POSTER_BASE_URL
+import kotlinx.android.synthetic.main.movie_list_item.view.*
+import com.selva.movie.data.repository.NetworkState
+import com.selva.movie.data.repository.Status
+import com.selva.movie.ui.single_movie_details.SingleMovie
+import com.selva.movie.ui.single_movie_details.SingleMovieViewModel
+import kotlinx.android.synthetic.main.network_state_item.view.*
 class PopularMoviePagedListAdapter: PagedListAdapter<Movie, RecyclerView.ViewHolder>(MovieDiffCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
