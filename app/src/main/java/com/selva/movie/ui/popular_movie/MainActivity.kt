927018f905aa5c5bb9e99592cc0d2d017b2574d8
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-19 09:42:03 +0500

    popularmoviepagedlistadapter added

--- app/src/main/java/com/selva/movie/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/selva/movie/ui/popular_movie/MainActivity.kt
@@ -3,11 +3,18 @@ package com.selva.movie.ui.popular_movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.selva.movie.R
+import com.selva.movie.data.api.TheMovieDBClient
+import com.selva.movie.data.api.TheMovieDBInterface
+import com.selva.movie.data.repository.NetworkState
 import com.selva.movie.ui.single_movie_details.SingleMovie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainActivityViewModel
@@ -16,7 +23,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+        movieRepository = MoviePagedListRepository(apiService)
+        viewModel = getViewModel()
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                if (viewType == movieAdapter.MOVIE_VIEW_TYPE) return 1
+                else return 3
+            }
+        };
 
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePagedList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()){
+                movieAdapter.setNetworkState(it)
+            }
+        })
     }
 
     private fun getViewModel(): MainActivityViewModel{
